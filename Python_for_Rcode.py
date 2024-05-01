8d1363123f461c70557d4b1a62432771aa4d729e
applesandberries <148832037+applesandberries@users.noreply.github.com>
2024-05-01 16:07:30 -0500

    Update Python_for_Rcode.py

--- Python_for_Rcode.py
+++ Python_for_Rcode.py
@@ -2,13 +2,14 @@ import subprocess
 
 def generate_ani_comparison_plot(A_to_A, B_to_B, A_to_B):
     # Run R script as a subprocess
-    subprocess.run(["Rscript", "/Downloads/pipline_rscript.R", A_to_A, B_to_B, A_to_B])
+    subprocess.run(["Rscript", f"{basic_path}/Pipeline_args.r", A_to_A, B_to_B, A_to_B])
 
 def main():
-    # Provide the paths to the three matrices here
-    A_to_A = "/Downloads/AtoA.txt"
-    B_to_B = "/Downloads/BtoB.txt"
-    A_to_B = "/Downloads/AtoB.txt"
+    parser = argparse.ArgumentParser(description='Generate ANI comparison plot')
+    parser.add_argument('A_to_A', type=str, help='Path to A_to_A matrix')
+    parser.add_argument('B_to_B', type=str, help='Path to B_to_B matrix')
+    parser.add_argument('A_to_B', type=str, help='Path to A_to_B matrix')
+    args = parser.parse_args()
 
     generate_ani_comparison_plot(A_to_A, B_to_B, A_to_B)
 
